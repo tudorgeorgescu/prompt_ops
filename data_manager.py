fcf897a1e9ca200c484cba8f83afd2dde9b4b5f7
tudorgeorgescu <tudorgeorgescu@gmail.com>
2025-11-23 16:39:27 +0100

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,9 @@
-
 import os
 import json
 import pandas as pd
 
-PROM load_data():PROMPTS_FILE = 'prompts.json'
-    df_input = pd.read_csv(INPUT_FILE) if os.path.exists(INPUT_FILE) else pd.DataFrame()
+PROMPTS_FILE = 'prompts.json'
+INPUT.read_csv(INPUT_FILE) if os.path.exists(INPUT_FILE) else pd.DataFrame()INPUT_FILE = 'source_data.csv'
     prompts_data = json.load(open(PROMPTS_FILE)) if os.path.exists(PROMPTS_FILE) else []
     schema_data = json.load(open(SCHEMA_FILE)) if os.path.exists(SCHEMA_FILE) else {}
     return df_input, prompts_data, schema_data
@@ -12,6 +11,6 @@ PROM load_data():PROMPTS_FILE = 'prompts.json'
 def save_prompts(prompts_list):
     with open(PROMPTS_FILE, 'w') as f:
         json.dump(prompts_list, f, indent=2)
-INPUT_FILE = 'source_data.csv'
 SCHEMA_FILE = 'schema_config.json'
 
+def load_data():
