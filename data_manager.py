11d1b8c1c3e2bb0fc26a67427aaf83efb172369b
tudorgeorgescu <tudorgeorgescu@gmail.com>
2025-11-23 16:40:43 +0100

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -8,9 +8,12 @@ INPUT.read_csv(INPUT_FILE) if os.path.exists(INPUT_FILE) else pd.DataFrame()INPU
     schema_data = json.load(open(SCHEMA_FILE)) if os.path.exists(SCHEMA_FILE) else {}
     return df_input, prompts_data, schema_data
 
+def load_data():
+    df_input = pd.read_csv(INPUT_FILE) if os.path.exists(INPUT_FILE) else pd.DataFrame()
+    prompts_data = json.load(open(PROMPTS_FILE)) if os.path.exists(PROMPTS_FILE) else []
+    schema_data = json.load(open(SCHEMA_FILE)) if os.path.exists(SCHEMA_FILE) else {}
+    return df_input, prompts_data, schema_data
+
 def save_prompts(prompts_list):
     with open(PROMPTS_FILE, 'w') as f:
         json.dump(prompts_list, f, indent=2)
-SCHEMA_FILE = 'schema_config.json'
-
-def load_data():
